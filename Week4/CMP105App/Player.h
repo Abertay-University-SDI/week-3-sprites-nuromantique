44f2c9e9efc3336dd47f03839da0cea0c7e0fbba
2400988 <bones@thewallace.co.uk>
2025-02-06 10:50:04 +0000

    created player.h but i dont know what im doing ngl

---
+++ Week4/CMP105App/Player.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Framework/GameObject.h"
+#include <iostream>
+#include <string.h>
+#include "Framework/Input.h"
+
+class Player : public GameObject {
+
+public: 
+
+	void handleInput();
+
+
+};
